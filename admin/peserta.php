5ca698ba1bbddafc0ac855a02fa79a07880a0747
NeuroZero <106090457+Graphene26@users.noreply.github.com>
2023-12-04 10:34:47 +0700

    upload_test

--- admin/peserta.php
+++ admin/peserta.php
@@ -13,13 +13,22 @@ include "../services/koneksi.php";
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="hasil.php">Quick Count</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="peserta.php">Daftar Peserta</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="calon.php">Daftar Calon</a>
+                    </li>
                     <li class="nav-item">
                         <a class="nav-link" href="action-logout.php">Logout</a>
                     </li>
